edc2aee2adcaaefd24472f82a5641d5043825dd6
qpaseo <162771007+qpaseo@users.noreply.github.com>
2024-10-14 21:21:36 +0900

    사이드바 코드
    사이드바만 있는 코드입니다, 사이드바를 불러올 위치에 적절히 넣어 사용하여 주세요

---
+++ components/common/sidebar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from "./indexStyle"
+
+const SideBar = () => {
+  return (
+    <S.Bar>
+      <S.NavFunc>
+      <S.Logo src="images/logo.svg"/>
+      <S.NavTxt>신청현황</S.NavTxt>
+      <S.NavTxt>대여현황</S.NavTxt>
+      </S.NavFunc>
+    </S.Bar>
+  )
+}
+
+export default SideBar
