3e779cd2fe0077593a5a65fab168f1ea1d080852
bbqcream <163371197+yzzs100@users.noreply.github.com>
2024-10-14 23:20:35 +0900

    main prototype #1

---
+++ src/components/common/sidebar/indexStyle.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Bar = styled.div`
+    min-height:100vh;
+    width:224px;
+    background-color:#D9D9D9;
+`
+
+export const NavFunc = styled.div`
+    height:203px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:space-around;
+`
+
+export const Logo = styled.img`
+    width:129px;
+    height:86px;
+`
+
+export const NavTxt = styled.div`
+    font-size:24px;
+`
